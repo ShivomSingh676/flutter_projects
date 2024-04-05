d285dea2b803fc6098d6c2728a130a25f7b4650c
Shivom Singh <shivompal676@gmail.com>
2024-04-05 21:15:44 +0530

    TakeCare

--- TakeCare/lib/main.dart
+++ TakeCare/lib/main.dart
@@ -1,45 +1,47 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_fcm1/firebase_options.dart';
+// import 'package:firebase_auth/firebase_auth.dart'; // Import Firebase Auth
+import 'package:flutter_fcm1/features/app/splash_screen/splash_screen.dart';
+import 'package:flutter_fcm1/features/user_auth/presentation/pages/home_page.dart';
+import 'package:flutter_fcm1/features/user_auth/presentation/pages/login_page.dart';
+import 'package:flutter_fcm1/features/user_auth/presentation/pages/sign_up_page.dart';
 
-void main() async{
+
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-);
-  runApp(const MyApp());
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: "AIzaSyCsHDQtI9DItQgSqwy45_y2xG9tDGxuER8",
+        appId: "1:540215271818:web:8b22d4aee01acdce862873",
+        messagingSenderId: "540215271818",
+        projectId: "flutter-firebase-9c136",
+        // Your web Firebase config options
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Hello"),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Firebase',
+      routes: {
+        '/': (context) => SplashScreen(
+          // Here, you can decide whether to show the LoginPage or HomePage based on user authentication
+          child: LoginPage(),
+        ),
+        '/login': (context) => LoginPage(),
+        '/signUp': (context) => SignUpPage(),
+        '/home': (context) => HomePage(),
+      },
     );
   }
 }
